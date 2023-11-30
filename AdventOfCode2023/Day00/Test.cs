071c80427b4f6434cf7aedd6690a75782f4afabf
Ådne Ovesen <adnewo@hotmail.com>
2023-11-30 22:17:08 +0100

    Add project files.

---
+++ AdventOfCode2023/Day00/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023.Day00
+{
+    public class Test
+    {
+        public static int GetNumberOfLines(string path)
+        {
+            return File.ReadAllLines(path).Length;
+        }
+    }
+}
---
+++ AdventOfCode2023/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AdventOfCode2023.Day00;
+
+var path = "..\\..\\..\\Day00\\sample.txt";
+Console.WriteLine($"Number of lines in sample.txt {Test.GetNumberOfLines(path)}");
